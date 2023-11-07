14b17839727f788b4352746de7e7c4372329c602
R4iw3r103 <stkin1003@gmail.com>
2023-11-08 00:13:00 +0900

    [add]13. アプリケーションまで完了

--- run.sh
+++ run.sh
@@ -17,10 +17,18 @@ CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib
 # -Wl, -Tkernel.ld: 隣家スクリプト指定
 # -Wl, -Map=kernel.map: マップファイルを出力
 
+OBJCOPY=/opt/homebrew/opt/llvm/bin/llvm-objcopy
+
+# シェルをビルド
+$CC $CFLAGS -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
+  # ビルドしたELFファイルを生バイナリ(ベースアドレスから実際にメモリ上に展開される内容が入ったもの)形式に変換
+$OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+  # 生バイナリ形式の実行イメージをC言語に埋め込める形式に変換する処理
+$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
 
 # カーネルをビルド
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-  kernel.c common.c
+  kernel.c common.c shell.bin.o
 
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
   -kernel kernel.elf
